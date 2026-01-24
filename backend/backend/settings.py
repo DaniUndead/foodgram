ba4a721f26d24c5ce18f3b8eec622d9ca70abfaa
Dani <dan04was@gmail.com>
2026-01-24 16:42:24 +0300

    next 1.7

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,6 +27,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
+    'djoser',
+    'recipes',
+    'users',
+    'api',
     'api.apps.ApiConfig',
 ]
 
