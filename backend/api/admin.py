af1c72e08dc9de4f5279dcf1285a5b0bbb1542e2
Dani <dan04was@gmail.com>
2026-01-24 17:00:32 +0300

    next 1.1

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -15,7 +15,7 @@ class TagAdmin(admin.ModelAdmin):
 
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
-    list_display = ('name')
+    list_display = ('name',)
     search_fields = ('name', 'unit')
 
 
