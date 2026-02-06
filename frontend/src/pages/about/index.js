fca6a93cf9dd221e31b998149ebce193c62ddbb1
Dani <dan04was@gmail.com>
2026-02-06 11:35:33 +0300

    NEW

--- frontend/src/pages/about/index.js
+++ frontend/src/pages/about/index.js
@@ -32,19 +32,6 @@ const About = ({ updateOrders, orders }) => {
             </p>
           </div>
         </div>
-        <aside>
-          <h2 className={styles.additionalTitle}>
-            Ссылки
-          </h2>
-          <div className={styles.text}>
-            <p className={styles.textItem}>
-              Код проекта находится тут - <a href="https://github.com/arefiture/foodgram" className={styles.textLink}>Github</a>
-            </p>
-            <p className={styles.textItem}>
-              Автор проекта: <a href="https://t.me/alexrinko" className={styles.textLink}>Раков Александр</a>
-            </p>
-          </div>
-        </aside>
       </div>
       
     </Container>
