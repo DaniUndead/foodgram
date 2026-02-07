b5b599790eeae5ece5a93509b80a0e387041ce27
Dani <dan04was@gmail.com>
2026-02-07 16:02:11 +0300

    gg 1.4

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import Group
 from django.utils.safestring import mark_safe
 
@@ -109,29 +108,3 @@ try:
     admin.site.unregister(User)
 except admin.sites.NotRegistered:
     pass
-
-
-@admin.register(User)
-class UserAdmin(RecipeCountMixin, BaseUserAdmin):
-    list_display = (
-        'id',
-        'username',
-        'full_name',
-        'email',
-        'following_count',
-        'followers_count',
-        *RecipeCountMixin.list_display
-    )
-    list_filter = ('is_staff', 'is_active')
-
-    @admin.display(description='ФИО')
-    def full_name(self, obj):
-        return f"{obj.first_name} {obj.last_name}".strip() or obj.username
-
-    @admin.display(description='Подписок')
-    def following_count(self, obj):
-        return obj.followers.count()
-
-    @admin.display(description='Подписчиков')
-    def followers_count(self, obj):
-        return obj.autors.count()
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,14 +1,34 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group
+from recipes.mixins import RecipeCountMixin
 
 from .models import User
 
+admin.site.unregister(Group)
+
 
 @admin.register(User)
-class UserAdmin(UserAdmin):
+class UserAdmin(RecipeCountMixin, BaseUserAdmin):
     list_display = (
-        'id', 'username', 'email',
-        'first_name', 'last_name'
+        'id',
+        'username',
+        'full_name',
+        'email',
+        'following_count',
+        'followers_count',
+        *RecipeCountMixin.list_display
     )
-    search_fields = ('email', 'username')
-    list_filter = ('email', 'username')
+    list_filter = ('is_staff', 'is_active')
+
+    @admin.display(description='ФИО')
+    def full_name(self, obj):
+        return f"{obj.first_name} {obj.last_name}".strip() or obj.username
+
+    @admin.display(description='Подписок')
+    def following_count(self, obj):
+        return obj.followers.count()
+
+    @admin.display(description='Подписчиков')
+    def followers_count(self, obj):
+        return obj.autors.count()
