37729da1792a2ac649d6f85e2c45481f12f5941f
Dani <dan04was@gmail.com>
2026-02-07 16:09:42 +0300

    gg 1.7

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.models import Group
 from recipes.mixins import RecipeCountMixin
 
 from .models import User
-
-admin.site.unregister(Group)
 
 try:
     admin.site.unregister(User)
